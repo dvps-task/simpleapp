518b2dd511429db7b639ea8a0a32e65d0e7d2fdc
dvps-task <localhost@localhost>
2017-04-15 07:15:39 +0000

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,24 @@
 # config valid only for current version of Capistrano
 lock "3.8.0"
 
-server "node1", roles: [:web, :app], primary: true
-
 set :application, "simpleapp"
-set :repo_url, "git@github.com:dvps-task/simpleapp.git"
-set :user, "vagrant"
+set :repo_url, "https://github.com/dvps-task/simpleapp.git"
+
+set :deploy_to, "/home/#{fetch(:local_user)}/apps/#{fetch(:application)}"
+
 set :puma_threads, [4, 16]
 set :puma_workers, 0
-
-# rbenv info
-#set :rbenv_custom_path, '/home/#{fetch(:user)}/.rbenv'
-#set :rbenv_ruby, '2.0.4'
-
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa_ansible) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to true if using ActiveRecord
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -77,3 +64,28 @@ namespace :deploy do
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
